f16b648260ec1d339cf9a36b40db8f29aa118aae
Siam <saabdull@microsoft.com>
2016-10-04 22:10:13 -0700

    asp.net core clr with cross platform and docker

---
+++ Startup.cs
@@ -0,0 +1,25 @@
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace AspnetCoreApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.Run(context =>
+            {
+                return context.Response.WriteAsync("Hello from ASP.NET Core!" + env.EnvironmentName);
+            });
+        }
+    }
+}
